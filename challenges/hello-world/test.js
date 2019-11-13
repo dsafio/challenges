e27db52337b01f6c4b87f2162bebe1364739f1f5
Jullia Chaves <julliapatricia@gmail.com>
2019-11-13 16:03:50 -0300

    Update tests to Jest syntax

--- challenges/hello-world/test.js
+++ challenges/hello-world/test.js
@@ -1,17 +1,15 @@
-const assert = require("assert");
 const helloWorld = require("./solution");
 
-/* eslint-env mocha */
 describe("hello-world", () => {
-  it("exports a function", () => assert.equal(typeof helloWorld, "function"));
-
-  it("greets the world when receiving no parameter", () => {
-    assert.equal(helloWorld(), "Hello, World!");
+  it("exports a function", () => {
+    expect(helloWorld).toBeInstanceOf(Function);
   });
-
+  it("greets the world when receiving no parameter", () => {
+    expect(helloWorld()).toBe("Hello, World!");
+  });
   it("greets someone when receiving his/her name", () => {
-    ["john", "jane", "josh"].forEach(name => {
-      assert.equal(helloWorld(name), `Hello, ${name}!`);
-    });
+    ["john", "jane", "josh"].forEach(name =>
+      expect(helloWorld(name)).toBe(`Hello, ${name}!`)
+    );
   });
 });
