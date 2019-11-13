15e3a347c6ef73056cb0b63cf6688ebfb1dfe2e1
Jullia Chaves <julliapatricia@gmail.com>
2019-11-13 14:55:19 -0300

    Replaces Standard with Prettier  (#16)
    * Adds Prettier
    
    * prettier --write ./**/*.js
    
    * Replaces Standard with Prettier on npm run lint
    
    * Adds a helper script format
    
    * npm rm standard

--- challenges/fizz-buzz/test.js
+++ challenges/fizz-buzz/test.js
@@ -1,22 +1,22 @@
-const assert = require('assert')
-const fizzBuzz = require('./solution')
+const assert = require("assert");
+const fizzBuzz = require("./solution");
 
 /* eslint-env mocha */
-describe('fizz-buzz', () => {
-  it('exports a function', () => assert.equal(typeof fizzBuzz, 'function'))
+describe("fizz-buzz", () => {
+  it("exports a function", () => assert.equal(typeof fizzBuzz, "function"));
 
   it('returns "Fizz" for numbers divisible by 3', () => {
-    [3, 6, 9].forEach(x => assert.equal(fizzBuzz(x), 'Fizz'));
-    [1, 2, 4].forEach(x => assert.equal(fizzBuzz(x), x))
-  })
+    [3, 6, 9].forEach(x => assert.equal(fizzBuzz(x), "Fizz"));
+    [1, 2, 4].forEach(x => assert.equal(fizzBuzz(x), x));
+  });
 
   it('returns "Buzz" for numbers divisible by 5', () => {
-    [5, 10, 20].forEach(x => assert.equal(fizzBuzz(x), 'Buzz'));
-    [7, 11, 16].forEach(x => assert.equal(fizzBuzz(x), x))
-  })
+    [5, 10, 20].forEach(x => assert.equal(fizzBuzz(x), "Buzz"));
+    [7, 11, 16].forEach(x => assert.equal(fizzBuzz(x), x));
+  });
 
   it('returns "Fizz Buzz" for numbers divisible by 3 and 5', () => {
-    [15, 30, 45].forEach(x => assert.equal(fizzBuzz(x), 'Fizz Buzz'));
-    [7, 11, 23].forEach(x => assert.equal(fizzBuzz(x), x))
-  })
-})
+    [15, 30, 45].forEach(x => assert.equal(fizzBuzz(x), "Fizz Buzz"));
+    [7, 11, 23].forEach(x => assert.equal(fizzBuzz(x), x));
+  });
+});
--- challenges/hello-world/test.js
+++ challenges/hello-world/test.js
@@ -1,17 +1,17 @@
-const assert = require('assert')
-const helloWorld = require('./solution')
+const assert = require("assert");
+const helloWorld = require("./solution");
 
 /* eslint-env mocha */
-describe('hello-world', () => {
-  it('exports a function', () => assert.equal(typeof helloWorld, 'function'))
+describe("hello-world", () => {
+  it("exports a function", () => assert.equal(typeof helloWorld, "function"));
 
-  it('greets the world when receiving no parameter', () => {
-    assert.equal(helloWorld(), 'Hello, World!')
-  })
+  it("greets the world when receiving no parameter", () => {
+    assert.equal(helloWorld(), "Hello, World!");
+  });
 
-  it('greets someone when receiving his/her name', () => {
-    ;['john', 'jane', 'josh'].forEach(name => {
-      assert.equal(helloWorld(name), `Hello, ${name}!`)
-    })
-  })
-})
+  it("greets someone when receiving his/her name", () => {
+    ["john", "jane", "josh"].forEach(name => {
+      assert.equal(helloWorld(name), `Hello, ${name}!`);
+    });
+  });
+});
