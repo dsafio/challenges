328f99979706dfeea4d1a54d3328da743a4132fd
Jullia Chaves <julliapatricia@gmail.com>
2019-11-21 16:39:36 -0300

    Migrate to Jest (#17)
    * npm i jest
    
    * Replaces Mocha with Jest on scripts test and tast:watch
    
    * Update tests to Jest syntax
    
    * Adds a verbose output to tests

--- challenges/fizz-buzz/test.js
+++ challenges/fizz-buzz/test.js
@@ -1,22 +1,19 @@
-const assert = require("assert");
 const fizzBuzz = require("./solution");
 
-/* eslint-env mocha */
 describe("fizz-buzz", () => {
-  it("exports a function", () => assert.equal(typeof fizzBuzz, "function"));
-
+  it("exports a function", () => {
+    expect(fizzBuzz).toBeInstanceOf(Function);
+  });
   it('returns "Fizz" for numbers divisible by 3', () => {
-    [3, 6, 9].forEach(x => assert.equal(fizzBuzz(x), "Fizz"));
-    [1, 2, 4].forEach(x => assert.equal(fizzBuzz(x), x));
+    [3, 6, 9].forEach(x => expect(fizzBuzz(x)).toBe("Fizz"));
+    [1, 2, 4].forEach(x => expect(fizzBuzz(x)).toBe(x));
   });
-
   it('returns "Buzz" for numbers divisible by 5', () => {
-    [5, 10, 20].forEach(x => assert.equal(fizzBuzz(x), "Buzz"));
-    [7, 11, 16].forEach(x => assert.equal(fizzBuzz(x), x));
+    [5, 10, 20].forEach(x => expect(fizzBuzz(x)).toBe("Buzz"));
+    [7, 11, 16].forEach(x => expect(fizzBuzz(x)).toBe(x));
   });
-
-  it('returns "Fizz Buzz" for numbers divisible by 3 and 5', () => {
-    [15, 30, 45].forEach(x => assert.equal(fizzBuzz(x), "Fizz Buzz"));
-    [7, 11, 23].forEach(x => assert.equal(fizzBuzz(x), x));
+  it('return "Fizz Buzz" for numbers divisible by 3 and 5', () => {
+    [15, 30, 45].forEach(x => expect(fizzBuzz(x)).toBe("Fizz Buzz"));
+    [7, 11, 23].forEach(x => expect(fizzBuzz(x)).toBe(x));
   });
 });
